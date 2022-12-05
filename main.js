47675aca386fb70470a0e1d2f8da3279a691cf7a
scttzwdrff <90075803+scttzwdrff@users.noreply.github.com>
2022-12-05 17:30:04 -0500

    clean up

--- main.js
+++
@@ -1,9 +0,0 @@
-pen.font = "30px Courier New";
-pen.lineWidth = 5;
-seedWorld();
-seedPlayer();
-canvas.addEventListener("mousedown", function (e) {
-    getMousePosition(canvas, e);
-});
-setInterval(plotLegalMoves, delay);
-//# sourceMappingURL=main.js.map
